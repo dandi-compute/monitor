78a66cd95ca0ddadddf253760b1c3cb336c5a179
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-12-14 16:28:26 -0500

    Change script to use squeue_to_md_table.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-squeue --format="%.10i    %15P    %40j    %10u    %.2t    %.10M    %.6D    %.4C    %20R" --me | ./test_script_3.sh > README.md
+squeue --format="%.10i    %15P    %40j    %10u    %.2t    %.10M    %.6D    %.4C    %20R" --me | ./squeue_to_md_table.sh > README.md
 git add README.md
 git commit --message "update" | true
 git push
