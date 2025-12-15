1ae768a07bdd4ba0851b48abeb09d0720e13907d
CodyCBakerPhD (Engaging) <cody.c.baker.phd@gmail.com>
2025-12-14 21:16:52 -0500

    raise limits on text

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 cd /orcd/data/dandi/001/all-dandi-compute/monitor
-squeue --format="%.10i    %15P    %40j    %10u    %.2t    %.10M    %.6D    %.4C    %20R" --me | ./squeue_to_md_table.sh > README.md
+squeue --format="%.20i    %25P    %40j    %10u    %.2t    %.10M    %.6D    %.4C    %20R" --me | ./squeue_to_md_table.sh > README.md
 git add README.md
 git commit --message "update" | true
 git push
